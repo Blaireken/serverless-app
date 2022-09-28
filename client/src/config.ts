57ba76f55bada0b1223745da3fbf69dc55dc0291
Ken Muriithi <muriithiken66@gmail.com>
2022-09-28 20:59:57 +0300

    project completion

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,8 @@
-const apiId = 'im1th8rr46';
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+const apiId = '09iyy0pnh1';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: 'dev-fjp-3wf9.us.auth0.com',
-  clientId: 'RzGQEDmJF4A5OznOSj7zIYBQ1zWTe1dT',
+  domain: 'dev-v8evzwmo.us.auth0.com',
+  clientId: '4ns49TyJGvCNYx2hQE5CS9jYXtdrEnbp',
   callbackUrl: 'http://localhost:3000/callback'
 };
